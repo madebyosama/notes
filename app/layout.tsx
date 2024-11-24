d62bc7c850d48fc9b915bccecd3f7f9a8a0e2c94
Muhammad Osama <madebyosama@gmail.com>
2024-11-24 15:07:28 +0500

    Feat (notes): Installable app

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <meta name='apple-mobile-web-app-title' content='Notes' />
+        <link rel='manifest' href='/site.webmanifest' />
+        <script
+          defer
+          src='https://cloud.umami.is/script.js'
+          data-website-id='9f2b2e03-0f3c-440b-961d-4336b3cd5ff0'
+        ></script>
+        <meta name='robots' content='noindex, nofollow' />
+      </head>
       <body className={myFont.className}>{children}</body>
     </html>
   );
