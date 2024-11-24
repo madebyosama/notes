90c557defc0dfba8d70b723f66a30d76459c4e95
Muhammad Osama <madebyosama@gmail.com>
2024-11-24 18:02:22 +0500

    Update: Remove Autofocus

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,6 @@ export default function Home() {
         <form className={styles.form}>
           <textarea
             contentEditable
-            autoFocus
             required
             className={styles.textarea}
             placeholder='Note'
